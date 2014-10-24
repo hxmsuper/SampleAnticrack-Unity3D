8acb666635f74898afaf75a14d84e4a9e89e51bc
xiumo <xiumo@unity3d.com>
2014-10-25 00:27:18 +0800

    first version
    simple to testing an encrypt function

---
+++ Version1.0/Assets/Codes/SimpleAntiCrack.cs
@@ -0,0 +1,14 @@
+﻿
+public class SimpleAntiCrack{
+
+	public const int m_antiKey = 123456;
+	 
+	public static string EncryptData(string data)
+	{
+		return m_antiKey^data;
+	}
+	public static string DecryptData(string data)
+	{
+		return data ^=m_antiKey;
+	}
+}
